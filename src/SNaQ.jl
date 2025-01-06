ff8747feeff1e17b6c1a0d135c542799b5402bf3
Nathan Kolbow <nkolbow@wisc.edu>
2025-01-06 14:52:40 -0600

    cleaning up unnecessary imports

--- src/SNaQ.jl
+++ src/SNaQ.jl
@@ -2,36 +2,27 @@ module SNaQ
 
     using Dates
     using Distributed
-    using LinearAlgebra: diag, I, logdet, norm, LowerTriangular, mul!, lmul!, rmul!,
-            Diagonal, cholesky, qr, BLAS
-    # alternative: drop support for julia v1.4, because LinearAlgebra.rotate! requires julia v1.5
-    # using LinearAlgebra # bring all of LinearAlgebra into scope
-    # import LinearAlgebra.rotate! # allow re-definition of rotate!
-    using Printf: @printf, @sprintf
+
+    using Printf: @printf
     using Random
-    using Statistics: mean, quantile, median
+    using Statistics: mean
 
     # other libraries, indicate compatible version in Project.toml
     using BioSequences
     using BioSymbols
-    using Combinatorics: combinations
     using CSV
     using DataFrames # innerjoin new in v0.21
     using DataStructures # for updateInCycle with priority queue
     using Distributions #for RateVariationAcrossSites
     using FASTX
-    using Functors: fmap
     using GLM # for the lm function
     using NLopt # for branch lengths optimization
     using StaticArrays
-    using StatsBase # sample, coef etc.
-    using StatsFuns # logsumexp, logaddexp, log2π, various cdf
+    using StatsBase # sample, etc.
     using StatsModels # re-exported by GLM. for ModelFrame ModelMatrix Formula etc
     using PhyloNetworks
 
     import Base: show
-    import GLM: ftest
-    import StatsModels: coefnames
 
     const DEBUGC = false # even more debug messages
     global CHECKNET = false # for debugging only
