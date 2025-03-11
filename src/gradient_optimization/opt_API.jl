33739925cf9819b7293bbd4250a135ba37b742bb
Nathan Kolbow <nkolbow@wisc.edu>
2025-03-11 15:12:36 -0500

    refactored to utilize new recursive equations

--- src/gradient_optimization/opt_API.jl
+++ src/gradient_optimization/opt_API.jl
@@ -4,31 +4,50 @@ include("CF_struct.jl")
 include("CF_math.jl")
 
 
-function optimize_bls!(net::HybridNetwork, quartet_eqns, observed_CFs; return_logPL::Bool=false)
-    narg = nopt_params(net)
-    ngamma = net.numhybrids
-    optmap, idxobjmap = generate_optimization_map(net)
 
-    #objective(fill(0.5, narg), fill(0.5, narg), net, idxobjmap, quartet_eqns, observed_CFs, optmap)
+
+function optimize_bls!(net::HybridNetwork, quartet_eqns, observed_CFs; return_logPL::Bool=false, α::Real=Inf)
+
+    narg = length(net.hybrid) + length(net.edge)
+    param_map = Dict{Int, Int}()
+    idx_obj_map = Dict{Int, Union{Node, Edge}}()
+    params = Array{Float64}(undef, narg)
+    LB = Array{Float64}(undef, narg)
+    UB = Array{Float64}(undef, narg)
+    max_ID = net.numedges
+
+    for (j, obj) in enumerate(vcat(net.hybrid, net.edge))
+        obj.number = max_ID + j
+        param_map[obj.number] = j
+        idx_obj_map[j] = obj
+
+        if typeof(obj) <: Node
+            params[j] = getparentedgeminor(obj).gamma
+            LB[j] = 0.0001
+            UB[j] = 0.9999
+        else
+            params[j] = obj.length
+            LB[j] = 0.0001
+            UB[j] = 15.0
+        end
+    end
+
+    blocks, _ = find_quartet_equations(net)
 
     opt = Opt(NLopt.LD_LBFGS, narg)
-    opt.lower_bounds = [(j <= ngamma) ? 0.0001 : 0.0001 for j=1:narg]
-    opt.upper_bounds = [(j <= ngamma) ? 0.9999 : 15.0 for j = 1:narg]
-    opt.maxeval = 75
-    NLopt.max_objective!(opt, (x, grad) -> objective(x, grad, net, idxobjmap, quartet_eqns, observed_CFs, optmap))
+    opt.lower_bounds = LB
+    opt.upper_bounds = UB
+    NLopt.max_objective!(opt, (x, grad) -> objective(x, grad, net, blocks, observed_CFs, idx_obj_map, α))
     (minf, minx, ret) = NLopt.optimize(opt, fill(0.5, narg))
 
-    if return_logPL
-        return get_params(net), compute_logPL(quartet_eqns, net.edge, observed_CFs)
-    end
-    return get_params(net)
+    return minx
 end
 optimize_bls!(net::HybridNetwork, oCFs; kwargs...) = optimize_bls!(net, find_quartet_equations(net)[1], oCFs; kwargs...)
 
 
-function objective(X::Vector{Float64}, grad::Vector{Float64}, N, idx_to_obj_map, q_eqns, obsCFs, opt_map)::Float64
+function objective(X::Vector{Float64}, grad::Vector{Float64}, net::HybridNetwork, blocks::AbstractArray, obsCFs, idx_obj_map, α)::Float64
     for j = 1:length(X)
-        obj::Union{PN.Edge, PN.Node} = idx_to_obj_map[j]
+        obj::Union{Node, Edge} = idx_obj_map[j]
         if typeof(obj) <: PN.Node
             E_major = getparentedge(obj)
             E_minor = getparentedgeminor(obj)
@@ -45,12 +64,18 @@ function objective(X::Vector{Float64}, grad::Vector{Float64}, N, idx_to_obj_map,
     end
 
     total_loss = 0.0
-    for ((eqns1, eqns2, eqns3), iter_oCF) in zip(q_eqns, obsCFs)
-        total_loss += iter_oCF.data[1] * log(compute_eCF(eqns1, N.edge))
-        total_loss += iter_oCF.data[2] * log(compute_eCF(eqns2, N.edge))
-        total_loss += iter_oCF.data[3] * log(compute_eCF(eqns3, N.edge))
+    iter_eCF::Float64 = 0.0
+    for j = 1:size(blocks)[1]
+        for k = 1:3
+            # Loss function
+            iter_eCF = compute_eCF(blocks[j,k], X, k, α)
+            total_loss += obsCFs[j].data[k] * log(iter_eCF)
+
+            # Gradient
+            block_derivs = compute_block_derivs(blocks[j,k], X, k, α)
+            grad .+= block_derivs .* block_derivs .* obsCFs[j].data[k] ./ iter_eCF
+        end
     end
-    grad .= compute_gradient(q_eqns, N.edge, opt_map, obsCFs; use_cache=true)
     return total_loss
 end
 
