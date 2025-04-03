1ad3456e407745e5c61b089bb9a4080ed30f8403
Nathan Kolbow <nkolbow@wisc.edu>
2025-04-03 12:41:46 -0500

    specified imports more carefully

--- src/gradient_optimization/CF_struct.jl
+++ src/gradient_optimization/CF_struct.jl
@@ -2,6 +2,7 @@ using PhyloNetworks
 const PN = PhyloNetworks;
 const Node = PN.Node;
 const Edge = PN.Edge;
+import Graphs
 import Graphs: a_star, SimpleGraphs, add_edge!   # for finding what edges contribute to internal edges; could remove at some point
 using Combinatorics     # for n choose 4, should definitely be removed later
 
