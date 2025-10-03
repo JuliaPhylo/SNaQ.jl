c342c62894c6f3d896361fe745d926cc25fa1f44
Nathan Kolbow <nkolbow@wisc.edu>
2025-10-03 10:45:14 -0500

    updated to use function that safely redirects output

--- test/test_bootstrap.jl
+++ test/test_bootstrap.jl
@@ -1,4 +1,5 @@
 # test the functions in src/bootstrap.jl
+include("test_output_helper.jl")
 
 exdir = joinpath(@__DIR__,"..","examples")
 # exdir = joinpath(dirname(pathof(PhyloNetworks)), "..","examples")
@@ -8,12 +9,10 @@ exdir = joinpath(@__DIR__,"..","examples")
 @testset "bootsnaq from quartet CF intervals" begin
 T=readnewick(joinpath(exdir,"startTree.txt"))
 datf=DataFrame(CSV.File(joinpath(exdir,"tableCFCI.csv")); copycols=false)
-originalstdout = stdout
-redirect_stdout(devnull) # requires julia v1.6
-bootnet = bootsnaq(T,datf,nrep=2,runs=1,seed=1234,filename="",Nfail=2,
-                   ftolAbs=1e-3,ftolRel=1e-3,xtolAbs=1e-4,xtolRel=1e-3,liktolAbs=0.01)
-redirect_stdout(originalstdout)
-@test size(bootnet)==(2,)
+bootnet = safely_redirect_output() do
+    bootsnaq(T,datf,nrep=2,runs=1,seed=1234,filename="",Nfail=2,
+                    ftolAbs=1e-3,ftolRel=1e-3,xtolAbs=1e-4,xtolRel=1e-3,liktolAbs=0.01)
+end
 @test writenewick(bootnet[1], round=true) != writenewick(bootnet[2], round=true)
 # "(2,((5,#H9:0.0::0.298):3.927,3):1.331,(((1,6):0.019,4):0.0)#H9:0.0::0.702);"
 # above: bad diamond 2, and both edges above the hybrid have estimated length of 0.0...
