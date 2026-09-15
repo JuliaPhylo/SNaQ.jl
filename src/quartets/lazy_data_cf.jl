f89612975f40d1d0243cfe49c528ae275c33c27a
Nathan Kolbow <nkolbow@wisc.edu>
2026-09-15 14:10:58 -0500

    implementing Base functions for LazyDataCF and LazyQuartetArray for better user experience

--- src/quartets/lazy_data_cf.jl
+++ src/quartets/lazy_data_cf.jl
@@ -6,31 +6,84 @@ Basically a helper struct that returns `Quartet` objects on demand
 mutable struct LazyQuartetArray{Quartet} <: AbstractVector{Quartet}
     quartets::Dict{Int,Quartet}
     lazyq::LazyQuartetCF
-    LazyQuartetArray(lq::LazyQuartetCF) = new{Quartet}(lq)
+    LazyQuartetArray(lq::LazyQuartetCF) = new{Quartet}(
+		Dict{Int,Quartet}(),
+		lq
+	)
 end
 
 function Base.getindex(lqa::LazyQuartetArray, i::Int)::Quartet
-    if has(quartets, i)
-        return quartets[i]
+	if haskey(lqa.quartets, i)
+        return lqa.quartets[i]
     end
 
     ocfs = lqa.lazyq[i, 1:3]
-    taxa = laq.lazyq.taxa[unrank4taxa(lazyq.ntaxa, i)]
+    taxa = lqa.lazyq.taxa[unrank4taxa(lqa.lazyq.ntaxa, i)]
 	lqa.quartets[i] = Quartet(
         i, taxa[1], taxa[2], taxa[3], taxa[4], ocfs
     )
 	return lqa.quartets[i]
 end
 
+function Base.getindex(lqa::LazyQuartetArray, idxs::AbstractVector{<:Int})::Vector{Quartet}
+	ret = Array{Quartet}(undef, length(idxs))
+	for (reti, idx) in enumerate(idxs)
+		ret[reti] = lqa[idx]
+	end
+	return ret
+end
+
+function Base.length(lqa::LazyQuartetArray)
+	return length(lqa.quartets)
+end
+
+function Base.size(lqa::LazyQuartetArray)
+	return (length(lqa.quartets),)
+end
+
+function Base.eachindex(lqa::LazyQuartetArray)
+	return collect(keys(lqa.quartets))
+end
+
 struct LazyDataCF
     quartet::LazyQuartetArray
 
-    # All the same parameters as DataCF *except* quartet (b/c we don't
-    # always have all quartets) and repSpecies, because
-    # this does not support multiple alleles at this point
-    numQuartets::Int
+    # Similar parameters to DataCF
+	taxa::Vector{String}
+	numQuartets::Int
     tree::Vector{HybridNetwork}
     numTrees::Int
 
-    LazyDataCF(trees::Vector{HybridNetwork}) = new(LazyQuartetArray(LazyQuartetCF(trees)))
+    function LazyDataCF(trees::Vector{HybridNetwork})
+		taxa = sort(reduce(union, tiplabels(t) for t in trees))
+		return new(
+			LazyQuartetArray(LazyQuartetCF(trees)),
+			taxa,
+			binomial(length(taxa), 4),
+			trees,
+			length(trees)
+		)
+	end
+end
+
+function Base.show(io::IO, ::MIME"text/plain", ldcf::LazyDataCF)
+	print(io, 
+	"""
+	LazyDataCF Object
+		$(ldcf.numTrees) gene trees with $(length(ldcf.taxa)) unique taxa
+		$(length(ldcf.quartet)) CFs computed out of $(ldcf.numQuartets) total possible
+	""")
+end
+
+function write(filename::String, ldcf::LazyDataCF)
+	open(filename, "w+") do f
+		write(f, "# $(join(ldcf.taxa, ","))\n")
+		write(f, "# $(ldcf.computedQuartets) computed out of $(ldcf.totalQuartets)\n")
+		write(f, "row,taxa1,taxa2,taxa3,taxa4,CF12_34,CF13_24,CF14_23\n")
+		for i in eachindex(ldcf.quartet)
+			taxa = ldcf.taxa[unrank4taxa(length(ldcf.taxa), i)]
+			ocfs = ldcf.quartet[i].obsCF
+			write(f, "$i,$(taxa[1]),$(taxa[2]),$(taxa[3]),$(taxa[4]),$(ocfs[1]),$(ocfs[2]),$(ocfs[3])\n")
+		end
+	end
 end
