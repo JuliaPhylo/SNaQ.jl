105f0f65b2157454704b5ba0fe27e89fbe356137
Nathan Kolbow <nkolbow@wisc.edu>
2026-09-22 12:05:05 -0500

    disallowed gatherCFmatrix with lazy DataCF

--- src/quartets/observed_cf.jl
+++ src/quartets/observed_cf.jl
@@ -116,10 +116,8 @@ in `dcf` ordered in the way that `SNaQ` expects internally.
 - `forcelazy::Bool=false`: whether or not to force collection with a lazy `DataCF` object.
     `false` by default because this could potentially be extremely overencumbering.
 """
-function gatherCFmatrix(dcf::DataCF; forcelazy::Bool=false)::Matrix{Float64}
-    dcf.lazy && !forcelazy && error("You are attempting to gather a full CF matrix with a lazy DataCF. " *
-        "Depending on the data you are using, this could take an extremely long time to complete. Use" *
-        "`gatherCFmatrix(dcf; forcelazy=true)` to ignore this warning and collect the matrix anyways.")
+function gatherCFmatrix(dcf::DataCF)::Matrix{Float64}
+    dcf.lazy && error("A full CF matrix cannot be gathered with a lazy DataCF object. Try again with `gatherCFmatrix(DataCF(genetrees))` instead.")
     # Helper function for more legible code later
     minmax(i1::Int, i2::Int)::Tuple{Int,Int} = (min(i1, i2), max(i1, i2))
 
