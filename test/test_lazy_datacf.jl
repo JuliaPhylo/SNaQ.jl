b992ccf9c8d8a5db871c7ae0132a289740db28f1
Nathan Kolbow <nkolbow@wisc.edu>
2026-09-22 11:39:27 -0500

    LazyDataCF -> SNaQ.LazyDataCF

--- test/test_lazy_datacf.jl
+++ test/test_lazy_datacf.jl
@@ -7,13 +7,13 @@ gts = simulatecoalescent(tre, 1000, 1);
 
 
 @testset "LazyDataCF read from file can compute SNaQ score, but not with new CFs" begin
-	ldcf = LazyDataCF(gts)
+	ldcf = SNaQ.LazyDataCF(gts)
 	s11 = computeSNaQscore!(tre, ldcf; propQuartets=0.1, seed=2)
 	s12 = computeSNaQscore!(tre, ldcf; propQuartets=0.3, seed=12)
 	s13 = computeSNaQscore!(tre, ldcf; numQuartets=15, seed=22)
 	write("ldcf.csv", ldcf)
 
-	ldcf = LazyDataCF("ldcf.csv")
+	ldcf = SNaQ.LazyDataCF("ldcf.csv")
 	s21 = computeSNaQscore!(tre, ldcf; propQuartets=0.1, seed=2)
 	s22 = computeSNaQscore!(tre, ldcf; propQuartets=0.3, seed=12)
 	s23 = computeSNaQscore!(tre, ldcf; numQuartets=15, seed=22)
@@ -27,11 +27,11 @@ gts = simulatecoalescent(tre, 1000, 1);
 end
 
 @testset "LazyDataCF can compute all SNaQ scores when it has all CFs" begin
-	ldcf = LazyDataCF(gts)
+	ldcf = SNaQ.LazyDataCF(gts)
 	computeSNaQscore!(tre, ldcf; propQuartets=1.0)
 	write("ldcf.csv", ldcf)
 
-	ldcf = LazyDataCF("ldcf.csv")
+	ldcf = SNaQ.LazyDataCF("ldcf.csv")
 	@test (computeSNaQscore!(tre, ldcf; numQuartets=40, seed=0); true)
 	@test (computeSNaQscore!(tre, ldcf; numQuartets=70, seed=1); true)
 	@test (computeSNaQscore!(tre, ldcf; propQuartets=0.25, seed=2); true)
